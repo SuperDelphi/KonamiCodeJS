b4bec9b4eee4634002bbd665c3bc092356d10b80
Simon <simon.breil42@gmail.com>
2020-02-20 11:29:38 +0100

    Added the main JS lib

---
+++ KonamiCodeListener.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020. Simon Breil
+ */
+
+class KonamiCodeListener
+{
+    constructor()
+    {
+        this.keySequence = [
+            "ArrowUp",
+            "ArrowUp",
+            "ArrowDown",
+            "ArrowDown",
+            "ArrowLeft",
+            "ArrowRight",
+            "ArrowLeft",
+            "ArrowRight",
+            "KeyB",
+            "KeyQ" // equals to "KeyA" on AZERTY keyboard layout
+        ];
+        this.maxTimeout = 1000;
+        this.currentPressedKeys = [];
+    }
+
+    listen(callback)
+    {
+        this.timeout = setTimeout(() => { this.currentPressedKeys = [] }, this.maxTimeout);
+        this.callback = callback;
+
+        window.addEventListener("keydown", ev => {
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => { this.currentPressedKeys = [] }, this.maxTimeout);
+            this._regKey(ev.code);
+        });
+    }
+
+    _regKey($keyCode)
+    {
+        this.currentPressedKeys.push($keyCode);
+        console.log(this.currentPressedKeys);
+        this._checkKeySequence();
+    }
+
+    _checkKeySequence()
+    {
+        if (this.currentPressedKeys.length >= this.keySequence.length) {
+            if (JSON.stringify(this.currentPressedKeys) === JSON.stringify(this.keySequence)) {
+                this.callback();
+            }
+            this.currentPressedKeys = [];
+        }
+    }
+}
